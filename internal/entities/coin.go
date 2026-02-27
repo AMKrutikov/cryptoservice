36cddce47e667bcc1cbe950b75395d54d9e8f3d4
AMKrutikov <andrewleo05.87@gmail.com>
2026-02-28 01:47:45 +0700

    add coin_test entieties

--- internal/entities/coin.go
+++ internal/entities/coin.go
@@ -1,12 +1,27 @@
 package entities
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Coin struct {
 	Title    string
-	Price    float32
+	Price    float64
 	ActualAT time.Time
 }
 
-// далее пишем контсруктор
-// далее пишем тесты
+func NewCoin(title string, price float64, actualAT time.Time) (*Coin, error) {
+	if strings.TrimSpace(title) == "" {
+		return nil, errors.New("Title cannot be empty")
+	}
+	if price <= 0 {
+		return nil, errors.New("Price cannot be zero or negative")
+	}
+	return &Coin{
+		Title:    title,
+		Price:    price,
+		ActualAT: actualAT,
+	}, nil
+}
---
+++ internal/entities/coin_test.go
@@ -0,0 +1,55 @@
+package entities
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+// Визуализируем вывод тестов в рамках пакета Coin
+func TestMain(m *testing.M) {
+	fmt.Println("TEST Package Coin")
+	res := m.Run()
+	fmt.Println("The end test Package Coin")
+
+	os.Exit(res)
+}
+
+func TestNewCoin(t *testing.T) {
+	t.Run("title empty", func(t *testing.T) {
+		title := ""
+		price := 100.00
+		actualAT := time.Now()
+
+		Coin, err := NewCoin(title, price, actualAT)
+		if Coin != nil {
+			t.Errorf("coin not validation: %v", err)
+		}
+
+	})
+
+	t.Run("price zero or negative", func(t *testing.T) {
+		title := "bitcoin"
+		price := -1.00
+		actualAT := time.Now()
+
+		Coin, err := NewCoin(title, price, actualAT)
+		if Coin != nil {
+			t.Errorf("coin not validation: %v", err)
+		}
+
+	})
+
+	t.Run("title empty AND price zero or negative", func(t *testing.T) {
+		title := " "
+		price := 0.00
+		actualAT := time.Now()
+
+		Coin, err := NewCoin(title, price, actualAT)
+		if Coin != nil {
+			t.Errorf("coin not validation: %v", err)
+		}
+	})
+
+}
