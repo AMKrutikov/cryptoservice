a74ed48220b06ff2b501bad1725ec10862dad4b0
AMKrutikov <andrewleo05.87@gmail.com>
2026-03-12 23:59:23 +0700

    test_4 Mockery coverage cases 16.7%

--- internal/cases/cases_test.go
+++ internal/cases/cases_test.go
@@ -7,9 +7,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/AMKrutikov/cryptoservice/internal/cases"
 	"github.com/AMKrutikov/cryptoservice/internal/cases/mocks"
 	"github.com/AMKrutikov/cryptoservice/internal/entities"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	// Пакеты из вашего go.mod
 	// Пути к вашим внутренним пакетам
 )
@@ -43,4 +46,24 @@ func TestCryptoProvider_Mock(t *testing.T) {
 	assert.NoError(t, err)                    // Ошибки быть не должно
 	assert.Equal(t, expectedCoins, result)    // Результаты должны совпадать
 	assert.Equal(t, "BTC", result[0].Title()) // Сравнение первого элемента в слайсе
+}
+
+func TestAAA(t *testing.T) {
+	t.Run("test number one", func(t *testing.T) {
+		mockCryptoProvider := mocks.NewCryptoProvider(t)
+		tmr := time.Now()
+		ctx := context.Background()
+		expectedTitles := []string{"A1", "A2", "A3"}
+		c1, _ := entities.NewCoin("A1", 0, tmr)
+		c2, _ := entities.NewCoin("A2", 0, tmr)
+		c3, _ := entities.NewCoin("A3", 0, tmr)
+		expectedCoin := []*entities.Coin{c1, c2, c3}
+
+		mockCryptoProvider.On("GetActualRates", mock.Anything, expectedTitles).
+			Return(expectedCoin, nil)
+
+		result, err := cases.AAA(ctx, expectedTitles, mockCryptoProvider)
+		require.NoError(t, err)
+		require.Equal(t, expectedCoin, result)
+	})
 }
--- internal/cases/provider.go
+++ internal/cases/provider.go
@@ -5,7 +5,12 @@ import (
 
 	"github.com/AMKrutikov/cryptoservice/internal/entities"
 )
+
 //go:generate mockery
 type CryptoProvider interface {
 	GetActualRates(ctx context.Context, titles []string) ([]*entities.Coin, error)
+}
+
+func AAA(ctx context.Context, titles []string, c CryptoProvider) ([]*entities.Coin, error) {
+	return c.GetActualRates(ctx, titles)
 }
