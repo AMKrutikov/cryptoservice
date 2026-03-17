9c8f7c5246f9f23cb80e42cdee2003a612d2f190
AMKrutikov <andrewleo05.87@gmail.com>
2026-03-17 22:02:36 +0700

    task 2/7

--- internal/cases/service.go
+++ internal/cases/service.go
@@ -20,10 +20,11 @@ func NewService(provider CryptoProvider, storage CryptoStorage) (*Service, error
 		storage:  storage,
 	}, nil
 }
+
 func (s *Service) GetLastRates(ctx context.Context, titles []string) ([]*entities.Coin, error) { // Получить последние цены
 	listCoinMap := make(map[string]struct{})
 	containsAll := true
-	// запршиваем список монет с базы (GetCoinsList) и сохраняем в переменную (storeTitles)
+	// запрашиваем список монет с базы (GetCoinsList) и сохраняем в переменную (storeTitles)
 	storeTitles, err := s.storage.GetCoinsList(ctx)
 	if err != nil {
 		return nil, entities.ErrStorage
@@ -32,13 +33,13 @@ func (s *Service) GetLastRates(ctx context.Context, titles []string) ([]*entitie
 	for _, elem := range storeTitles {
 		listCoinMap[elem] = struct{}{}
 	}
-	//   проверяем есть ли в базе(storage) запрашиваемые монеты(titles),
+	// проверяем есть ли в базе(storage) запрашиваемые монеты(titles)
 	for _, elem := range titles {
 		if _, exists := listCoinMap[elem]; !exists {
 			containsAll = false
 		}
 	}
-	// - если есть, выводим слайс монет!
+	// если есть, выводим слайс монет
 	if containsAll {
 		resultCoins, err := s.storage.GetActualCoins(ctx, titles)
 		if err != nil {
@@ -47,12 +48,12 @@ func (s *Service) GetLastRates(ctx context.Context, titles []string) ([]*entitie
 		return resultCoins, nil
 	}
 
-	//если нет запрашиваем у провайдера
+	// если нет запрашиваем у провайдера
 	providerCoins, err := s.provider.GetActualRates(ctx, titles)
 	if err != nil {
 		return nil, entities.ErrProvider
 	}
-	// и сохраняем в базу!
+	// и сохраняем в базу
 	err = s.storage.Store(ctx, providerCoins)
 	if err != nil {
 		return nil, entities.ErrStorage
