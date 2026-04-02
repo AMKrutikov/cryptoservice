77c2b2db4d3a57dd5f43e50b8eeac8903a24477f
AMKrutikov <andrewleo05.87@gmail.com>
2026-04-02 13:28:51 +0700

    editing client.go

--- internal/adapter/provider/client.go
+++ internal/adapter/provider/client.go
@@ -12,28 +12,38 @@ import (
 	"github.com/pkg/errors"
 )
 
-type ProviderClient struct {
-	apiAuthorization string
-	apiKey           string
-	//apiAuthorization := "x-cg-demo-api-key"
-	//apiKey := "CG-SdGMn7C5Rv2F4hTMwLdJ1Pk6"
+// type Client struct {
+// 	*http.Client
+// }
+
+type coinGeckoClient struct {
+	apiAuthorization string //apiAuthorization := "x-cg-demo-api-key"
+	apiKey           string //apiKey := "CG-SdGMn7C5Rv2F4hTMwLdJ1Pk6"
+	vs_currencies    string // usd
+	client           *http.Client
+	//client           *http.Client
 }
 
-func NewProviderClient(apiAuthorization string, apiKey string) (*ProviderClient, error) {
+func NewProviderClient(apiAuthorization string, apiKey string, vs_currencies string) (*coinGeckoClient, error) {
 	if strings.TrimSpace(apiAuthorization) == "" {
 		return nil, errors.Wrap(entities.ErrInvalidParam, "apiAuthorization cannot be empty")
 	}
 	if strings.TrimSpace(apiKey) == "" {
 		return nil, errors.Wrap(entities.ErrInvalidParam, "apiKey cannot be empty")
 	}
-	return &ProviderClient{
+	if strings.TrimSpace(vs_currencies) == "" {
+		return nil, errors.Wrap(entities.ErrInvalidParam, "vs_currencies cannot be empty")
+	}
+	return &coinGeckoClient{
 		apiAuthorization: apiAuthorization,
 		apiKey:           apiKey,
+		vs_currencies:    strings.ToLower(vs_currencies),
+		//client:           client,
 	}, nil
 }
 
-func (p *ProviderClient) GetActualRates(ctx context.Context, titles []string) ([]*entities.Coin, error) {
-
+func (с *coinGeckoClient) GetActualRates(ctx context.Context, titles []string) ([]*entities.Coin, error) {
+	///
 	if len(titles) == 0 {
 		return nil, errors.Wrap(entities.ErrInvalidParam, "empty titles")
 	}
@@ -43,46 +53,41 @@ func (p *ProviderClient) GetActualRates(ctx context.Context, titles []string) ([
 		qureyTitles[idx] = strings.ToLower(elem)
 	}
 
-	url := buildUrl(qureyTitles, "usd")
+	url := buildUrl(qureyTitles, с.vs_currencies)
 
 	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return nil, errors.Wrap(entities.ErrInvalidParam, "request error")
+		return nil, errors.Wrapf(entities.ErrInternal, "request error: %v", err)
 	}
-	request.Header.Add(p.apiAuthorization, p.apiKey)
+	request.Header.Add(с.apiAuthorization, с.apiKey)
+	///
 
-	response, err := http.DefaultClient.Do(request)
+	response, err := http.DefaultClient.Do(request) // создать в конструкторе , добавить паттерн функциональные опции для конфигурирования таймаута клиента
+	//response, err := c.
+
 	if err != nil {
-		return nil, errors.Wrap(entities.ErrInvalidParam, "response error")
+		return nil, errors.Wrap(entities.ErrInternal, "response error")
 	}
 	defer response.Body.Close()
 
-	parsedResponse := make(map[string]struct {
-		Usd float64 `json:"usd"`
-	})
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.Wrap(entities.ErrInternal, "provider api error")
+	}
+	///
+	parsedResponse := make(map[string]map[string]float64)
 
 	if err := json.NewDecoder(response.Body).Decode(&parsedResponse); err != nil {
-		return nil, errors.Wrap(entities.ErrInvalidParam, "invalid JSON response")
+		return nil, errors.Wrap(entities.ErrInternal, "invalid JSON response")
 	}
-	//
-	// body, err := io.ReadAll(response.Body)
-	// if err != nil {
-	// 	return nil, errors.Wrap(entities.ErrInvalidParam, "error reading response body")
-	// }
-
-	// if err := json.Unmarshal(body, &parsedResponse); err != nil {
-	// 	return nil, errors.Wrap(entities.ErrInvalidParam, "invalid JSON response")
-	// }
-	//
 
 	resultCoins := make([]*entities.Coin, 0, len(titles))
+	tmNow := time.Now()
 	for title, elem := range parsedResponse {
-		coin, err := entities.NewCoin(title, elem.Usd, time.Now())
-		if err != nil {
-			errors.Wrapf(entities.ErrInvalidParam, "coin creation error: %s\n", title) //???
-			continue
+		if price, ok := elem[с.vs_currencies]; ok {
+			if coin, err := entities.NewCoin(title, price, tmNow); err == nil {
+				resultCoins = append(resultCoins, coin)
+			}
 		}
-		resultCoins = append(resultCoins, coin)
 	}
 
 	return resultCoins, nil
