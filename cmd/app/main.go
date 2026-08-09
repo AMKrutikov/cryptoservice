d13f770a111ecad7886d033740fa116a15e3ac8e
AMKrutikov <andrewleo05.87@gmail.com>
2026-08-09 16:56:21 +0700

    add Graceful Shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/AMKrutikov/cryptoservice/internal/adapter/config"
 	"github.com/AMKrutikov/cryptoservice/pkg/application"
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
 	path := os.Getenv("PATH_CONFIG")
 	if strings.TrimSpace(path) == "" {
 		path = "config/cryptoservice.yaml"
@@ -19,5 +24,11 @@ func main() {
 	cnfg := config.NewConfig(path)
 	app := application.NewApplication(cnfg)
 	app.Run(ctx)
+
+	<-ctx.Done()
+	fmt.Println("Main: Received shutdown signal")
+
+	app.Stop()
+	fmt.Println("Main: Application stopped cleanly")
 
 }
--- internal/cases/storage.go
+++ internal/cases/storage.go
@@ -12,4 +12,6 @@ type CryptoStorage interface {
 	GetCoinsList(ctx context.Context) ([]string, error)                                               // список имен монет, которые представлены
 	GetActualCoins(ctx context.Context, titles []string) ([]*entities.Coin, error)                    // получение последних монет по title-ам
 	GetAggregateCoins(ctx context.Context, titles []string, aggType string) ([]*entities.Coin, error) // Агрегированный запрос над монетами
+
+	Close()
 }
